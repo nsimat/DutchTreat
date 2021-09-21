c2b182a58a1f5ec73dacd9b2acd7742e1fff1cbb
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2021-09-21 20:28:32 +0200

    Adding the Account controller and Login View

---
+++ DutchTreat/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+
+            return View();
+        }
+    }
+}
